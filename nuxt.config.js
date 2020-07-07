c07c85355cc5e30ff991b8c048c042f93e71f84d
Sindre Bøyum <sindre.boyum@gmail.com>
2020-07-07 12:23:54 +0200

    Set 🥰 as the favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,14 +7,15 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: pkg.name,
+    title: 'How to emoji - Learn how to use ✨emoji✨ on your device 🤩',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: pkg.description }
+
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', href: 'data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🥰</text></svg>' }
     ]
   },
 
