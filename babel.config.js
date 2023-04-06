00e6797f3c59a0f4733c1b8946e6cba81d9636d9
Sindre Bøyum <sindre.boyum@tietoevry.com>
2023-04-06 12:06:46 +0200

    refactor: use nuxt@3

--- babel.config.js
+++ babel.config.js
@@ -1,14 +1,5 @@
 module.exports = function (api) {
   api.cache(true);
 
-  const presets = [ ];
-  const plugins = [ 
-    '@babel/plugin-proposal-optional-chaining',
-    '@babel/plugin-proposal-nullish-coalescing-operator',
-  ];
-
-  return {
-    presets,
-    plugins
-  };
-}
+  return {};
+};
